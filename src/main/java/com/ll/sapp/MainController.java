c67e3a339515e6f431b73382c85d4a3ed5693c84
FWWKCS <ckstlr1607@gmail.com>
2024-07-15 15:26:26 +0900

    work

---
+++ src/main/java/com/ll/sapp/MainController.java
@@ -0,0 +1,14 @@
+package com.ll.sapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @ResponseBody
+    @GetMapping("/sapp")
+    public String index() {
+        return "안녕";
+    }
+}
